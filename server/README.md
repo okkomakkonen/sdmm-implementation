f2dea4b1e8e61f2efa7f7d31f9d674420ced5da3
Okko Makkonen <okko@okkomakkonen.fi>
2022-04-27 16:15:30 +0300

    Add slow multiplication method to server

--- server/README.md
+++ server/README.md
@@ -2,49 +2,12 @@
 
 The SDMM server is a basic API for multiplying matrices with the help of helper servers.
 
-## Get the docker image
+## Running the server
+
+The server can be run using
 
 ```bash
-docker pull okkomakkenen/sdmm-server
+python server.py --port 5000 --processes 25
 ```
 
-## Run the server with docker locally
-
-Start the server in a docker container
-
-```bash
-docker run -p 5000:5000 okkomakkonen/sdmm-server
-```
-
-## Deploy locally with minikube
-
-Install minikube and set the vm-driver.
-
-Start minikube with
-
-```bash
-minikube start
-```
-
-Deploy the cluster
-
-```bash
-sh deploy.sh
-```
-
-Configure the /etc/hosts file to use the ip address of the cluster
-
-```bash
-echo "$(minikube ip) sdmm.server" | sudo tee -a /etc/hosts
-```
-
-or edit directly with
-```bash
-sudo vim /etc/hosts
-```
-
-To delete the minikube cluster, first stop the server and then delete the cluster
-```bash
-minikube stop
-minikube delete
-```
+The `--port` argument specifies the port to run on and `--processes` specifies how many processes can be used to handle the requests.
--- server/server.py
+++ server/server.py
@@ -3,47 +3,13 @@
 from typing import Dict, Any
 
 import json
-import base64
 import argparse
 
 from flask import Flask, request
-import galois  # type: ignore
-import numpy as np  # type: ignore
+
+import sdmm.utils
 
 app = Flask(__name__)
-
-
-def serialize_np_array(A: np.ndarray) -> Dict[str, Any]:
-
-    data = A.tobytes()
-    shape = A.shape
-    dtype = A.dtype.name
-
-    d = {
-        "data": base64.b64encode(data).decode("utf-8"),
-        "shape": shape,
-        "dtype": dtype,
-    }
-
-    if hasattr(type(A), "order"):
-        d["order"] = type(A).order
-
-    return d
-
-
-def deserialize_np_array(d: Dict[str, Any]) -> np.ndarray:
-
-    dtype = np.dtype(d["dtype"])
-    data = base64.b64decode(d["data"])
-    shape = d["shape"]
-
-    A = np.reshape(np.frombuffer(data, dtype=dtype), newshape=shape)
-
-    if "order" in d:
-        F = galois.GF(d["order"])
-        A = F(A)
-
-    return A
 
 
 @app.route("/")
@@ -62,22 +28,43 @@ def multiply():
     # parse input
     data = request.get_json()
 
-    A = deserialize_np_array(dict(data["A"]))
-    B = deserialize_np_array(dict(data["B"]))
+    A = sdmm.utils.deserialize_np_array(dict(data["A"]))
+    B = sdmm.utils.deserialize_np_array(dict(data["B"]))
 
     # perform multiplication
-    C = A @ B
+    C = sdmm.utils.fast_multiply(A, B)
 
     # encode the result and send it
-    CE = serialize_np_array(C)
+    CE = sdmm.utils.serialize_np_array(C)
 
     return json.dumps(CE)
 
+@app.route("/slow_multiply", methods=["POST"])
+def slow_multiply():
+    """Multiply the matrices and return the product
+
+    Takes input as b64 encoded numpy arrays
+    """
+
+    # parse input
+    data = request.get_json()
+
+    A = sdmm.utils.deserialize_np_array(dict(data["A"]))
+    B = sdmm.utils.deserialize_np_array(dict(data["B"]))
+
+    # perform multiplication
+    C = sdmm.utils.slow_multiply(A, B)
+
+    # encode the result and send it
+    CE = sdmm.utils.serialize_np_array(C)
+
+    return json.dumps(CE)
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="Serve SDMM multiplication server")
     parser.add_argument("--port", default=5000, help="Port to run server on")
+    parser.add_argument("--processes", default=1, help="Number of processes to use")
     args = parser.parse_args()
 
-    app.run(host="0.0.0.0", port=args.port)
+    app.run(host="0.0.0.0", port=args.port, debug=False, processes=int(args.processes), threaded=False)
