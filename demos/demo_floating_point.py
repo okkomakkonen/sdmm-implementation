311d0c0f00fa8c907072164cf4e1ae98035a1554
Okko Makkonen <okko@okkomakkonen.fi>
2022-05-11 12:33:19 +0300

    Updating demo

--- demos/demo_floating_point.py
+++ demos/demo_floating_point.py
@@ -5,8 +5,15 @@ import numpy as np
 from sdmm import MatDotFloatingPoint
 from sdmm.utils import fake_multiply, fast_multiply, slow_multiply
 
+# If SLOW = True, then the multiplication will be performed using the slow matrix multiplication, which is not optimized
+# If SLOW = False, then the multiplication will be performed using the optimized matrix multiplication algorithm in numpy
+SLOW = False
+
+# Urls of the servers to use
+# The server instances have to be running at this address for this demo to work
 urls = ["http://localhost:5000"] * 25
 
+# Initiaizing the secure MatDot instance
 matdot = MatDotFloatingPoint(
     num_partitions=10,
     num_colluding=1,
@@ -15,15 +22,20 @@ matdot = MatDotFloatingPoint(
     std_a=1.0,
     std_b=1.0,
     threaded=True,
-    slow_multiplication=False,
+    slow_multiplication=SLOW,
 )
 
-(t, s, r) = (2000, 2000, 2000)
+# The sizes of the matrices
+# A is t x s and B is s x r
+(t, s, r) = (400, 400, 400)
 
+# Drawing random matrices to use for the multiplication
 A = np.random.normal(loc=0.0, scale=1.0, size=(t, s))
 B = np.random.normal(loc=0.0, scale=1.0, size=(s, r))
 
 print(f"Multiplying {t} x {s} and {s} x {r} matrices")
+
+# ----- COMPUTING USING MATDOT -----
 print("Computing using secure MatDot over floating point")
 start = time()
 
@@ -31,13 +43,18 @@ C = matdot(A, B)
 
 print(f"Took \033[1m{time() - start:.3f}s\033[m")
 
+# ----- COMPUTING LOCALLY -----
 print("Computing locally")
 start = time()
 
-AB = fast_multiply(A, B)
+if SLOW:
+    AB = slow_multiply(A, B)
+else:
+    AB = fast_multiply(A, B)
 
 print(f"Took \033[1m{time() - start:.3f}s\033[m")
 
+# Checking the result
 if np.isclose(AB, C).all():
     print("\033[32;1mcorrect product\033[m")
 else:
